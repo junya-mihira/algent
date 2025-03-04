265679a654a2d38f764602e88651c4f898238434
Junya Mihira <junyamihira@gmail.com>
2025-03-04 22:46:35 +0900

    match.py modified

--- src/match.py
+++ src/match.py
@@ -1,11 +1,24 @@
-def match(nums:list[int])->bool:
-    s=nums[0]
+def match(nums: list[int]) -> bool:
+    s = nums[0]
     for i in nums[1::]:
-        if i==s:
+        if i == s:
             return True
-        s=i
-if __name__=='__main__':
-    nums=list(range(10))
-    nums1=[0,1,2,3,4,5,5]
-    ans=match(nums1)
+        s = i
+
+
+def match1(nums: list[int]) -> bool:
+    s = []
+    s.append(nums[0])
+    for i in nums[1::]:
+        if i in s:
+            return True
+        s.append(i)
+
+
+if __name__ == '__main__':
+    nums = list(range(10))
+    nums1 = [0, 1, 2, 3, 4, 5, 5]
+    ans = match(nums1)
     print(ans)
+    ans1 = match1(nums1)
+    print(ans1)
